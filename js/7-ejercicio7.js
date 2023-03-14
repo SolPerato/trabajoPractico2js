88f615dcc2ae6f24f326b8bf02027b2e73b66406
Sol Perato <solperato27@gmail.com>
2023-03-13 23:55:46 -0300

    Ejercicio7- ingresa una cantidad de numeros y forma una piramide invertida

---
+++ js/7-ejercicio7.js
@@ -0,0 +1,30 @@
+/*7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30).
+
+303030303030303030303030303030303030303030303030303030303030
+2929292929292929292929292929292929292929292929292929292929
+28282828282828282828282828282828282828282828282828282828
+…..
+333
+22
+1
+*/
+
+
+var numrep = prompt("Introduce numero de repeticiones");
+if (Number(numrep) == numrep) {
+    if (numrep > 0 && numrep <= 50) {
+        var rep,i;
+        for (i = numrep; i >= 1; i--) {
+            for (rep = i; rep >= 1; rep--) {
+                document.write(i);
+            }
+            document.write("<br>");
+        }
+    } 
+    else {
+        alert("El número introducido no es válido");
+    }
+} 
+else {
+    alert("No has introducido un número");
+}
